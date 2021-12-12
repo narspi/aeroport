5cc7d7c92c52d056d0158435fd5ae3ba9e2d2e3f
narspi <ivanovmichurina@gmail.com>
2021-12-12 09:30:09 +0300

    smoth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const menuBurger = document.getElementById("menu-burger");
   const modal = document.getElementById("modal");
+  const menu = document.getElementById("menu");
+
+  function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+      return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+  }
+
+  function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+      node = node.offsetParent;
+      y += node.offsetTop;
+    }
+    return y;
+  }
+
+  function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+      scrollTo(0, stopY);
+      return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+      for (var i = startY; i < stopY; i += step) {
+        setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+        leapY += step;
+        if (leapY > stopY) leapY = stopY;
+        timer++;
+      }
+      return;
+    }
+    for (var i = startY; i > stopY; i -= step) {
+      setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+      leapY -= step;
+      if (leapY < stopY) leapY = stopY;
+      timer++;
+    }
+  }
+
+  menu.addEventListener('click',event=>{
+    event.preventDefault();
+    const target = event.target;
+    let isAnchor = false;
+    if (target.href) {
+      const hash = new URL(target.href).hash.substring(1);
+      if (hash) {
+        event.preventDefault();
+        smoothScroll(hash);
+      }
+    }
+  })
 
   menuBurger.addEventListener("click", () => {
     menuBurger.classList.toggle("active");
@@ -13,11 +79,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.body.style.overflow === "hidden") {
       document.body.style.overflow = "";
       document.body.style.paddingRight = "0px";
-    }
-    else {
+    } else {
       document.body.style.overflow = "hidden";
       document.body.style.paddingRight = scrollbarWidth + "px";
-    } 
+    }
   });
 
   let videoSlider = new Swiper(".reviews__video", {
@@ -64,9 +129,9 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!fixedBlock.classList.contains("show")) {
           if (currentPageYOffset > window.pageYOffset) {
             fixedBlock.classList.add("show");
-            setTimeout(()=>{
+            setTimeout(() => {
               fixedBlock.classList.remove("show");
-            },3000)
+            }, 3000);
           }
         } else {
           if (currentPageYOffset < window.pageYOffset) {
@@ -83,7 +148,7 @@ document.addEventListener("DOMContentLoaded", () => {
     currentPageYOffset = window.pageYOffset;
   });
 
-  const accordionAnimation = document.getElementById("accordion-animation");
+  const accordionAnimation = document.getElementById("faq");
 
   accordionAnimation.addEventListener("click", (event) => {
     const target = event.target;
